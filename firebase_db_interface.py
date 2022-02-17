122e83a87ff0204f3c6d0e22cdc0b7a3a2f2ef03
Seth Hamilton <seth.hamilton801@gmail.com>
2022-02-16 22:11:55 -0600

    initial stab at a firebase db interface

---
+++ firebase_db_interface.py
@@ -0,0 +1,37 @@
+from db_interface import DBInterface
+import requests
+import shutil
+import os
+
+class FirebaseDBInterface(DBInterface):
+    """
+    Interface for firebase
+
+    download_bot_file: 
+    """
+    
+    def __init__():
+        super().__init__()
+
+    def configure(self, record):
+        self.player = {}
+        self.player.name = record["name"]
+        self.player.type = DBInterface.SEEKER if record["type"] == "seeker" else DBInterface.HIDER
+        self.player.link = record["link"] 
+
+        self.opponents = {}
+        for r in record["opponents"]:
+            self.opponents[r["name"]] =  r["link"]     
+
+    def get_opponents(self, type):
+        return self.opponents.keys()
+
+    def download_bot_file(self, name, type, destination):
+        if type == self.player.type:
+            url = self.player.link   
+        else:
+            url = self.opponents[name]
+        
+        r = requests.get(url)
+        with open(os.path.join(destination, f"{name}.py"), 'wb') as f:
+            f.write(r.content) 
